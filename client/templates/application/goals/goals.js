8d11f4e6a4b97a126f544e0ba2e39644472157c2
freegyes <freegyes@gmail.com>
2015-03-29 15:38:35 +0200

    clean up templates and add google analytics tracking | this fixes #21

--- client/templates/application/goals/goals.js
+++ client/templates/application/goals/goals.js
@@ -127,38 +127,6 @@ Template.goals.events({
       });   
     }
   },
-  'click .deleteGoal': function() {
-    Goals.remove(this._id);
-    Notifications.warn('Goal removed', 'Goal\'s expired and gone to meet its maker.');
-  },
-  'click .setGoalName': function(e, tmpl) {
-    // add goal id to session
-    Session.set('editing-goal', this._id);
-    bootbox.prompt({
-      title: "Edit the name of your goal:",
-      value: Goals.findOne({_id: Session.get('editing-goal')}).name,
-      callback: function(result) {
-        if (result === null) {
-          Session.set('editing-goal', null);
-        } else {
-           Goals.update(Session.get('editing-goal'), {$set: {name: result}});     
-           Notifications.success('Changed name', 'The name of the goal was changed successfully')
-        }
-      }
-    });
-  },
-  'click .setStatusReached': function() {
-    Goals.update(this._id, {$set: {status: "reached"}})
-  },
-  'click .setStatusPartiallyReached': function() {
-    Goals.update(this._id, {$set: {status: "partially-reached"}})
-  },
-  'click .setStatusNotReached': function() {
-    Goals.update(this._id, {$set: {status: "not-reached"}})
-  },
-  'click .setStatusDefault': function() {
-    Goals.update(this._id, {$set: {status: "default"}})
-  },
   'click .time-frame': function() {
     Session.set('view', this.timeFrame);
   },
---
+++ client/templates/application/projects/projects_list.js
@@ -0,0 +1,35 @@
+Template.projectsList.created = function() {
+  Meteor.subscribe('projects');
+}
+
+Template.projectsList.helpers({
+  projects: function(status) {     
+    if (Projects.find({status: status}).count() === 0) {
+      return false;
+    } else {
+      return Projects.find({status: status});
+    }
+  }
+});
+
+Template.projectsList.events({
+  'keyup .project-name': function(e, tmpl) {
+    // if enter is pressed then add project to db
+    if (e.which == 13 && $('.project-name').val()) {
+      var projectAttributes = {
+        name: $('.project-name').val()
+      };
+
+      Meteor.call('projectInsert', projectAttributes, function(error, result) {
+        // show notifications
+        if (error) Notifications.error('Something is not right.', error.reason);
+        Notifications.success('Project added', projectAttributes.name + ' has been successfully added.');
+        // set project active in the session
+        Session.set('projectId', result._id);
+        // reset input 
+        $('.project-name').val('');
+
+      });   
+    }
+  }
+});
